fddbd7c28eff6404460ccbd772f5d7bbcd59393d
wagner maidson <wagner.rmaidson@gmail.com>
2021-04-26 20:11:50 -0300

    create homepage for podcastr

--- podcastrnext/src/components/Player/index.tsx
+++ podcastrnext/src/components/Player/index.tsx
@@ -11,7 +11,33 @@ export function Player()
           <div className={styles.emptyPlayer}>
               <strong>Selecione um podast para ouvir</strong>
           </div>
-          <footer></footer>
+          <footer className={styles.empty}>
+              <div className={styles.progress}>
+                  <span>00:00</span>
+                  <div className={styles.slider}>
+                    <div className={styles.emptySlider} />
+                  </div>
+                  <span>00:00</span>
+              </div>
+              <div className={styles.buttons}>
+                  <button type="button">
+                      <img src="/shuffle.svg" alt="Embaralhar" />
+                  </button>
+                  <button type="button">
+                      <img src="/play-previous.svg" alt="Tocar anterior" />
+                  </button>
+                  <button type="button" className={styles.playButton}>
+                      <img src="/play.svg" alt="Tocar" />
+                  </button>
+                  <button type="button">
+                      <img src="/play-next.svg" alt="Tocar próxima" />
+                  </button>
+                  <button type="button">
+                      <img src="/repeat.svg" alt="Repetir" />
+                  </button>
+
+              </div>
+          </footer>
       </div>
     );
 }
